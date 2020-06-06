8faf89d8aa16ce5a5983518eb0d71fef3c737cdb
Colocust <516100080@qq.com>
2020-06-06 14:42:49 +0800

    完善filter

--- tiny/MongoDB/Filter.php
+++ tiny/MongoDB/Filter.php
@@ -5,5 +5,26 @@ namespace Tiny\MongoDB;
 
 
 class Filter {
+  private $filter = [];
 
+  public function where(string $field, string $op, $value): self {
+    $class = new Op($op, $value);
+    $this->filter[$field] = $class->getValue();
+    return $this;
+  }
+
+  public function in(string $field, array $value): self {
+    $this->filter[$field] = ['$in' => $value];
+    return $this;
+  }
+
+  public function or(string $field, string $op, $value): self {
+    $class = new Op($op, $value);
+    $this->filter['$or'][] = [$field => $class->getValue()];
+    return $this;
+  }
+
+  public function getFilter(): array {
+    return $this->filter;
+  }
 }
