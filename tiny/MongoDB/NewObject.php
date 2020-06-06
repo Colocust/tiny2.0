2b096081d6229bc84ebd0e2e72edec224d5c91a4
Colocust <516100080@qq.com>
2020-06-06 14:49:16 +0800

    添加newobject类

--- tiny/MongoDB/NewObject.php
+++ tiny/MongoDB/NewObject.php
@@ -5,5 +5,44 @@ namespace Tiny\MongoDB;
 
 
 class NewObject {
+  private $newObject = [];
 
+  public function set(string $field, $value): self {
+    $this->newObject['$set'][$field] = $value;
+    return $this;
+  }
+
+  public function inc(string $field, int $value): self {
+    $this->newObject['$inc'][$field] = $value;
+    return $this;
+  }
+
+  public function unset(string $field): self {
+    $this->newObject['$unset'][$field] = 1;
+    return $this;
+  }
+
+  public function push(string $field, $value): self {
+    $this->newObject['$push'][$field] = $value;
+    return $this;
+  }
+
+  protected function addToSet(string $field, $value): self {
+    $this->newObject['$addToSet'][$field] = $value;
+    return $this;
+  }
+
+  protected function pop(string $field, $value): self {
+    $this->newObject['$pop'][$field] = $value;
+    return $this;
+  }
+
+  protected function pull(string $field, $value): self {
+    $this->newObject['$pull'][$field] = $value;
+    return $this;
+  }
+
+  public function getNewObject(): array {
+    return $this->newObject;
+  }
 }
