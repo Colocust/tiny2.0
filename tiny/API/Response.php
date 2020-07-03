7e771c599977aae215f437a10027bc88d22f646b
Colocust <516100080@qq.com>
2020-07-03 13:43:30 +0800

    添加msg属性

--- tiny/API/Response.php
+++ tiny/API/Response.php
@@ -11,6 +11,7 @@ class Response {
 
   public $data;
   public $httpStatus;
+  public $httpStatusMsg;
   public $httpHeaders = [
     'Access-Control-Allow-Origin' => '*',
     'Access-Control-Allow-Headers' => 'Referer,Origin, Content-Type, Cookie, Accept,User-Agent',
