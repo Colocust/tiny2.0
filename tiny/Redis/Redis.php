1bec0bf0d94865ff10231ca7cf7dede7a6125d9c
Colocust <516100080@qq.com>
2020-07-03 20:43:57 +0800

    修改Redis类 不采用从连接池获取实例的方式

--- tiny/Redis/Redis.php
+++ tiny/Redis/Redis.php
@@ -3,27 +3,20 @@
 
 namespace Tiny;
 
-
-use Tiny\Pool\Redis\RedisPool;
 use Tiny\Redis\Config;
 
 class Redis {
-  /**
-   * @var $config Config
-   */
-  private $config;
 
   protected function __construct(Config $config) {
-    $this->config = $config;
-    //此段代码需要优化
-    //当前没有连接池实例时需要适当延迟再次获取
-    $this->db = RedisPool::get($config->host, $config->port);
+    $this->db = new \Redis();
+    $res = $this->db->pconnect($config->host, $config->port, $config->timeout);
+    if (!$res) {
+      Logger::getInstance()->fatal("connect redis(" . $config->host . ":" . $config->port . ") error");
+      throw new \Exception("connect redis error");
+    }
     $this->db->select($config->dbNo);
   }
 
-  public function __destruct() {
-    RedisPool::return($this->config->host, $this->config->port, $this->db);
-  }
 
   protected $db;
 
