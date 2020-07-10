d32b985bacb84101c11db3e7dd9171b0e2949642
Colocust <516100080@qq.com>
2020-07-10 13:55:33 +0800

    修改集成器

--- Integrator.php
+++ Integrator.php
@@ -9,27 +9,24 @@ require_once "tiny/Helper/Time.php";
 require_once "tiny/Loader/Loader.php";
 require_once "tiny/Annotation/File.php";
 
+use Tiny\Annotation\Property;
+use Tiny\Foundation\Server\API;
+
 Loader::register();
 
 define('__ROOT__', __DIR__);
-define('EXT', 'php');
+define('PHP_EXT', 'php');
+define('DOC_EXT', 'txt');
 
-/**
- *
- *
- * 敬畏每一行代码
- *
- * 集成器
- *
- * 生成类名与文件路径的映射关系
- *
- *
- */
 class Integrator {
   private static $classMap = [];
   private static $filterDirectory = ['.', '..', '.idea'];
 
   public function go() {
+    $shell = 'rm -rf __ClassLoader__.php';
+    shell_exec($shell);
+    $shell = 'rm -rf doc';
+    shell_exec($shell);
     //第一步 扫描所有文件 生成文件与类的对应关系
     $this->scanDir();
 
@@ -43,9 +40,13 @@ class __ClassLoader__ {
   public static $classMap = ' . $classMap . ';
 }';
     fwrite($file, $content);
+
+    //第二步 生成接口文档
+    mkdir('doc');
+    $this->scanClass();
   }
 
-  private function scanDir(string $dir = "") {
+  private function scanDir(string $dir = ""): void {
     $currentDir = __ROOT__;
     if ($dir) {
       $currentDir .= $dir;
@@ -70,23 +71,126 @@ class __ClassLoader__ {
     }
   }
 
-  private function addClassMap(string $dir) {
+  private function addClassMap(string $dir): void {
     $fileDir = __ROOT__ . $dir;
 
     $pathInfo = pathinfo($fileDir);
-    if (@$pathInfo['extension'] !== EXT) {
+    if (@$pathInfo['extension'] !== PHP_EXT) {
       return;
     }
 
-    $fileInstance = new File($fileDir);
-    $namespace = $fileInstance->getNamespace();
-    $class = $fileInstance->getClass();
+    try {
+      $file = new File($fileDir);
+    } catch (\Exception $e) {
+      return;
+    }
 
-    $className = $namespace . '\\' . $class;
+    $namespace = $file->getNamespace();
+    $class = $file->getClass();
 
-    self::$classMap[$className] = $dir;
+    if (is_null($namespace) || is_null($class)) {
+      return;
+    }
+
+    self::$classMap[$namespace . '\\' . $class] = $dir;
   }
 
+  private function scanClass(): void {
+    foreach (self::$classMap as $class => $dir) {
+      try {
+        $reflectionClass = new \ReflectionClass($class);
+      } catch (\ReflectionException $e) {
+        continue;
+      }
+
+      $class = $reflectionClass;
+      do {
+        $parentClass = $class->getParentClass();
+        if (!$parentClass) {
+          break;
+        }
+        if ($parentClass->getName() === API::class) {
+          $this->generateInterfaceDoc($reflectionClass);
+          break;
+        }
+        $class = $parentClass;
+      } while (true);
+    }
+  }
+
+  private function generateInterfaceDoc(\ReflectionClass $reflectionClass): void {
+    if ($reflectionClass->isAbstract()) {
+      return;
+    }
+    /**
+     * @var $api API
+     */
+    $api = $reflectionClass->newInstanceWithoutConstructor();
+
+    $docs = ['URL: ' . get_class($api) . PHP_EOL . PHP_EOL];
+
+    $rules = $this->getPropertiesRules($api);
+    foreach ($rules as $rule) {
+      $docs[] = $rule['name'] . ' {' . PHP_EOL;
+      $properties = $rule['properties'];
+
+      /**
+       * @var $property Property
+       */
+      foreach ($properties as $property) {
+        $docs[] = "   {$property->getName()} : {$property->getType()->getTypeName()} @{$property->getUses()->getName()}" . PHP_EOL;
+      }
+      $docs[] = '} ' . PHP_EOL . PHP_EOL;
+    }
+
+    $roots = explode('\\', get_class($api));
+    array_shift($roots);
+    $length = count($roots);
+    $dir = 'doc';
+    foreach ($roots as $key => $root) {
+      $dir .= DIRECTORY_SEPARATOR . $root;
+      if ($key == $length - 1) {
+        $dir .= '.' . DOC_EXT;
+        $file = fopen($dir, 'w');
+      }
+
+      if (!file_exists($dir)) {
+        mkdir($dir);
+      }
+    }
+    foreach ($docs as $doc) {
+      fwrite($file, $doc);
+    }
+  }
+
+  private function getPropertiesRules(API $api): array {
+    $stack[] = $api->responseClass();
+    $stack[] = $api->requestClass();
+
+    $rules = [];
+    while (!empty($stack)) {
+      $reflectionClass = new \ReflectionClass(array_pop($stack));
+      $reflectionProperties = $reflectionClass->getProperties();
+
+      $properties = [];
+      foreach ($reflectionProperties as $reflectionProperty) {
+        $property = new Property($reflectionProperty);
+
+        $type = $property->getType();
+        if ($type->isUserDefinedClass()) {
+          $stack[] = $type->getTypeName();
+        }
+        if ($type->isArray() && $type->getElementType()->isUserDefinedClass()) {
+          $stack[] = $type->getElementType()->getTypeName();
+        }
+        $properties[] = $property;
+      }
+      $rule['name'] = $reflectionClass->getName();
+      $rule['properties'] = $properties;
+      $rules[] = $rule;
+    }
+    return $rules;
+  }
 }
 
 $integrator = new Integrator();
