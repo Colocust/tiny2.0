65cedf84afc57c8f8f9a363c0c275b8e975b2a2b
Colocust <516100080@qq.com>
2020-08-19 14:34:05 +0800

    添加配置类

--- tiny/Kernel/Config.php
+++ tiny/Kernel/Config.php
@@ -5,5 +5,43 @@ namespace Tiny;
 
 
 class Config {
+    private $config = [];
 
+    public function get(string $name) {
+        $names = explode('.', $name);
+        $config = $this->config;
+        foreach ($names as $str) {
+            if (isset($config[$str])) {
+                $config = $config[$str];
+            } else {
+                throw new \Exception('wrong config name' . $str);
+            }
+        }
+        return $config;
+    }
+
+    public function load() {
+        $dir = __ROOT__ . '/config';
+        $this->parseFile($dir);
+    }
+
+    private function parseFile(string $dir): void {
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            $pathInfo = pathinfo($file);
+            if (@$pathInfo['extension'] !== PHP_EXT) {
+                continue;
+            }
+
+            $values = include $dir . "/$file";
+            $this->set($pathInfo['filename'], $values);
+        }
+    }
+
+    private function set(string $key, array $values): void {
+        $this->config[$key] = $values;
+    }
+
+    private function __construct() {
+    }
 }
